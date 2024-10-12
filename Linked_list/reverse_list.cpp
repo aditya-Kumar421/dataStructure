f7a9c20e187fe29562e97e556147b24e9f68ce3e
Aditya <ak.adityakumar421@gmail.com>
2024-10-12 22:36:02 +0530

    reverse a singly linked list

---
+++ Linked_list/reverse_list.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL || head->next == NULL) return head;
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        while (temp != NULL) {
+            ListNode* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+};
+
+void printList(ListNode* head) {
+    ListNode* temp = head;
+    while (temp != NULL) {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout << "Original List: ";
+    printList(head);
+
+    Solution solution;
+    ListNode* reversedHead = solution.reverseList(head);
+
+    cout << "Reversed List: ";
+    printList(reversedHead);
+
+    return 0;
+}
